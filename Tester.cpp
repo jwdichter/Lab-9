8e4d83221079c89526b30ec5b203339989b8549c
jwdichter <justin.dichter@richmond.edu>
2018-11-04 14:11:50 -0500

    Update Tester.cpp

--- Tester.cpp
+++ Tester.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main() {
-	cout << "----------LINKED LIST TESTER----------" << endl;
+  cout << "----------LINKED LIST TESTER----------" << endl;
 	
-	cout << "Testing constructors put/getters: " << endl;
-	cout << "-------------------------" << endl;
+  cout << "Testing constructors put/getters: " << endl;
+  cout << "-------------------------" << endl;
 
-	cout << "Linked List intList, Linked List stringList" << endl;
-	LinkedList intList;
-  LinkedList stringList
+  cout << "Linked List intList, Linked List stringList" << endl;
+  LinkedList intList;
+  LinkedList stringList;
 	
   cout << "------------Testing Add/Get/Remove/Size-------------"  << endl;
   cout << "Int:"<< endl;
@@ -36,41 +36,47 @@ int main() {
   cout << "stringList.add(\"B\")" << endl;
   cout << "stringList.get(1): " << stringList.get(1) << " [B]" << endl;
   cout << "stringList.add(\"C\")" << endl;
-  cout << "stringList.get(2): " << stringList.get(2) << " [C]" << endl;  
+  cout << "stringList.get(2): " << stringList.get(2) << " [C]" << endl;
+	
+  
   
   cout << "------------Testing Illegal Indices-------------"  << endl;
   
-  cout << "Index > size-1" << endl;
+  cout << "------Index > size-1-------" << endl;
+  cout << "Int:" << endl;
   cout << "intList.get(10): ";
-   try {
+  try {
      intList.get(10);
    }
-   catch (const std::invalid_argument& ie) {
+  catch (const std::invalid_argument& ie) {
      cerr << "\tInvalid Argument error: " << ie.what() << endl;
+  }
+	
+  cout << "String:" << endl;
+  cout << "stringList.get(10): ";
+  try {
+     stringList.get(10);
    }
-  
-   std::cout << "\tiv.get(3): ";
-   try {
-     iv.get(3);
+  catch (const std::invalid_argument& ie) {
+     cerr << "\tInvalid Argument error: " << ie.what() << endl;
+  }   
+	
+  cout << "------Index < 0-------" << endl;
+  cout << "Int:" << endl;
+  cout << "intList.get(-1): ";
+  try {
+     intList.get(-1);
    }
-   catch (const std::out_of_range& oor) {
-     std::cerr << "\tOut of Range error: " << oor.what() << std::endl;
+  catch (const std::invalid_argument& ie) {
+     cerr << "\tInvalid Argument error: " << ie.what() << endl;
+  }
+	
+  cout << "String:" << endl;
+  cout << "stringList.get(-1): ";
+  try {
+     stringList.get(-1);
    }
-   // test oor get() for negative index
-   std::cout << "\tiv.get(-1): ";
-   try {
-     iv.get(-1);
-   }
-   catch (const std::out_of_range& oor) {
-     std::cerr << "\tOut of Range error: " << oor.what() << std::endl;
-   }
-
-   std::cout << "--------------" << std::endl;
-   std::cout << "Testing size()"  << std::endl;
-   std::cout << "\t\tSize: "   << iv.size() << " [3]" << std::endl;
-   iv.put(90);
-   std::cout << "iv.put(90):" << "\tSize: " << iv.size() << " [4]" << std::endl;
-
-   std::cout << "--------------" << std::endl;
-
+  catch (const std::invalid_argument& ie) {
+     cerr << "\tInvalid Argument error: " << ie.what() << endl;
+  }  
 }
